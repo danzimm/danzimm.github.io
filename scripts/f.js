951544a77f84c0d14ab27fba54040ab6af846d4c
Dan Zimmerman <daniel.zimmerman@me.com>
2015-05-12 18:13:42 -0500

    mobilized the armies

--- scripts/f.js
+++ scripts/f.js
@@ -17,5 +17,98 @@ var F = (function() {
       return cont;
     }
   };
+  that.compose = function(f, g, thisArg) {
+    return function() {
+      return f(g.apply(thisArg !== undefined ? thisArg : this, arguments));
+    }
+  };
+  that.partialApply = function(f, x) {
+    return function() {
+      return f.apply(this, [x].concat(Array.prototype.slice.call(arguments)));
+    };
+  };
+  that.curry = function(f, i) {
+    if (i === undefined)
+      return that.curry(f, f.length);
+    if (i <= 1) {
+      return f;
+    }
+    return function(x) {
+      return that.curry(that.partialApply(f, x), i-1);
+    };
+  };
+  that.id = function(x) {
+    return x;
+  };
+  that.const = function(x) {
+    return function() {
+      return x;
+    };
+  };
+  that.flip = function(f) {
+    return (x, y) => {
+      return f(y, x);
+    };
+  };
+  that.plus = function(a, b) {
+    return a + b;
+  };
+  that.minus = function(a, b) {
+    return a - b;
+  };
+  that.times = function(a, b) {
+    return a * b;
+  };
+  that.divide = function(a, b) {
+    return a / b;
+  };
+
+  that.cons = (x, xs) => {
+    return [x].concat(xs);
+  };
+  that.concat = (xs, ys) => {
+    return concat.apply(xs, ys);
+  };
+  that.flatten = (xs) => {
+    F.ListMonad.join(xs);
+  };
+
+  that.Functor = function(fmap) {
+    this.fmap = fmap;
+  };
+  that.Monad = function(ret, bind) {
+    this.ret = ret;
+    this.bind = bind;
+  };
+  that.Monad.prototype.fmap = function(f, ma) {
+    return this.bind(ma, F.compose(this.ret, f));
+  };
+  that.Monad.prototype.join = function(mma) {
+    return this.bind(mma, F.id);
+  };
+  that.Applicative = function(pure, apply) {
+    this.pure = pure;
+    this.apply = apply;
+  };
+  that.Applicative.prototype.fmap = function(f, fa) {
+    return this.apply(this.pure(f), fa);
+  };
+  // It's hard to actually do functonal things here because you
+  // can't really embed your own type system since there isn't
+  // really a type system in the first place...
+  //
+  // Dunno if any of this works, just wrote it cuz seemed fun
+  // and was good practice.
+  
+  that.FunctionMonad = new that.Monad(that.const, function(ma, comp) {
+    return function(r) {
+      return comp(ma(r))(r);
+    };
+  });
+  that.ListMonad = new that.Monad(that.curry(that.flip(that.cons))([]), function(ma, comp) {
+    return Array.from(ma).map(comp).reduce(F.concat);
+  });
+
   return that;
 })();
+
--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,15 @@ window.onload = function() {
   // {{{initialize variables
   var selected = 1000;
   var transitioning = false;
+
   var header = document.querySelector('section header h1');
-  var elms = document.querySelectorAll('nav ul li'), i;
+  var menuItems = document.querySelectorAll('nav ul li'), i;
   var nav = document.querySelector('nav');
+  var frontPageItems = [].slice.call(menuItems).concat(header);
+
   var article = document.querySelector('article');
-  var transitionDuration = Z.convertPlainTextDurationToMS(F.takeUntil(window.getComputedStyle(elms[0])['transitionDuration'], ','));
+
+  var transitionDuration = Z.convertPlainTextDurationToMS(F.takeUntil(window.getComputedStyle(frontPageItems[0])['transitionDuration'], ','));
   var articleTransitionDuration = Z.convertPlainTextDurationToMS(F.takeUntil(window.getComputedStyle(article)['transitionDuration'], ','));
   // }}}
 
@@ -19,11 +23,14 @@ window.onload = function() {
 Variations (to give a preview of my knowledge base, I'm familiar with the base ideas in measure theory and functional analysis. I'm also familiar with all the basic theorems
  of Calculus of Variations, e.g. The du Bois-Reymond lemma etc. and Sobolev Spaces as well as the notion of weak solutions). I am self taught in CS but am familiar with many 
 topics that are taught in an undergrad CS program.`);
+
   sections.push(`As I'm sure many of the readers of this have realized (well, if I ever get a 'many' number of readers...), hobbies change from day to day, new ones are 
 picked up and old ones thrown away, and maybe even an old one reintroduced! But what I can say is that I enjoy programming. I think it's become even more than a hobby for me,
  but I still categorize it as that since that's how it all started out (yes, I have had a job as a Software Engineer, that's why it's a bit weird to call programming simply a
  hobby. Anyways I'm particularly curious, nowadays, with functional programming. I'm looking at you haskell/scala/elm. I have yet to get into lisp yet, but I'm sure I will 
 some day. I'm also curious about data science; 0xFEEDFACF and 0xDEADBEEF are useful.`);
+
+  sections.push(`Stuff will be put here eventually... ¯\\_(ツ)_/¯`);
   // }}}
 
   // {{{hide and show functions
@@ -39,22 +46,14 @@ some day. I'm also curious about data science; 0xFEEDFACF and 0xDEADBEEF are use
       }
       article.innerHTML = text;
       transitioning = true;
-      Z.enQ(Z.iterative(elms.length + 1, function(i) {
-        if (i < elms.length) {
-          elms[i].classList.remove('showing');
-        } else {
-          header.classList.remove('opaque');
-        }
-      })).then(Z.delay(transitionDuration)).then(Z.iterative(elms.length + 1, function(i) {
-        if (i < elms.length) {
-          elms[i].classList.add('displaynone');
-        } else {
-          header.classList.add('displaynone');
-        }
+      Z.enQ(Z.iterative(frontPageItems, function(elem) {
+        elem.classList.remove('showing');
+      })).then(Z.delay(transitionDuration)).then(Z.iterative(frontPageItems, function(elem) {
+        elem.classList.add('hidden');
       })).then(function() {
-        nav.classList.add('displaynone');
-        article.classList.remove('displaynone'); 
+        nav.classList.add('hidden');
         article.classList.remove('hidden'); 
+        article.classList.add('showing'); 
       }).then(Z.delay(articleTransitionDuration)).then(function() {
         transitioning = false;
       });
@@ -67,19 +66,16 @@ some day. I'm also curious about data science; 0xFEEDFACF and 0xDEADBEEF are use
     if (selected !== -1) {
       selected = -1;
       transitioning = true;
-      nav.classList.remove('displaynone');
+      nav.classList.remove('hidden');
       Z.enQ(function() {
+        article.classList.remove('showing');
+      }).then(Z.delay(articleTransitionDuration)).then(function() {
         article.classList.add('hidden');
-      }).then(Z.iterative(elms.length + 1, function(i) {
-        if (i < elms.length) {
-          elms[i].classList.remove('displaynone');
-          elms[i].classList.add('showing'); // TODO: unify 'opaque', 'showing', 'hidden'
-        } else {
-          header.classList.remove('displaynone');
-          header.classList.add('opaque');
-        }
+      }).then(Z.iterative(frontPageItems, function(elem) {
+        elem.classList.remove('hidden');
+        elem.classList.add('showing');
       })).then(Z.delay(transitionDuration)).then(function() {
-        article.classList.add('displaynone');
+        article.innerHTML = "";
         transitioning = false;
       });
     }
@@ -87,18 +83,26 @@ some day. I'm also curious about data science; 0xFEEDFACF and 0xDEADBEEF are use
   // }}}
   
   // {{{initialize document
-  Z.enQ(Z.delay(1000)).then(Z.iterative(document.querySelectorAll('#circle path'), function(elm, cb) {
-    elm.style.strokeOpacity = '1';
-    SVGHelpers.animatePath(elm, { duration: '1s' }).then(cb);
-  })).then(showMenu);
+  
+  if (document.body.clientWidth >= 600) {
+    Z.enQ(Z.delay(1000)).then(Z.iterative(document.querySelectorAll('#circle path'), function(elm, cb) {
+      elm.style.strokeOpacity = '1';
+      SVGHelpers.animatePath(elm, { duration: '1s' }).then(cb);
+    })).then(showMenu);
+  } else {
+    showMenu(); // TODO: i don't like initial animation on mobile, make it instant
+  }
+
   //}}}
   
   // {{{initialize click handlers
-  Z.enQ(Z.iterative(elms.length, function(j) {
-    elms[j].onclick = showArticle.bind(elms[j], j);
+
+  Z.enQ(Z.iterative(menuItems.length, function(j) {
+    menuItems[j].onclick = showArticle.bind(menuItems[j], j);
   }));
   document.querySelector('main').onclick = function(event) { event.stopPropagation(); };
-  document.onclick = showMenu;
+  document.onclick = header.onclick = showMenu;
+
   // }}}
 };
 
